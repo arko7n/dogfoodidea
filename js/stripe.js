47fe4688b592f9e32e3895887755bcbfaca6305e
EC2 Default User <ec2-user@ip-172-31-62-60.us-west-2.compute.internal>
2021-10-09 20:05:36 +0000

    initial stripe integration

---
+++ js/stripe.js
@@ -0,0 +1,40 @@
+var WildRydes = window.WildRydes || {};
+WildRydes.map = WildRydes.map || {};
+
+(function stripeWrapper($) {
+    
+    // Register click handler for #request button
+    $(function onDocReady() {
+        $('#checkoutButton').click(createCheckoutSession);
+    });
+    
+    function handleRequestClick(event) {
+        createCheckoutSession();
+    }
+    
+    function createCheckoutSession() {
+        $.ajax({
+            method: 'POST',
+            url: 'https://749ruwi05l.execute-api.us-west-2.amazonaws.com/prod/checkout-sessions',
+            data: JSON.stringify({
+                orderContext: "Blah blah"
+            }),
+            contentType: 'application/json',
+            success: proceedToStripeCheckout,
+            error: function ajaxError(jqXHR, textStatus, errorThrown) {
+                console.error('Error requesting ride: ', textStatus, ', Details: ', errorThrown);
+                console.error('Response: ', jqXHR.responseText);
+                alert('An error occured when requesting your unicorn:\n' + jqXHR.responseText);
+            }
+        });
+    }
+    
+    function proceedToStripeCheckout(stripeCheckoutSession) {
+        var unicorn;
+        var pronoun;
+        console.log('Response received from API: ', stripeCheckoutSession);
+
+        // Redirect customer
+        window.location = stripeCheckoutSession.url;
+    }
+}(jQuery));
