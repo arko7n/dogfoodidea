b7da20af9b724cf2156630baf922b10510c5cfe0
EC2 Default User <ec2-user@ip-172-31-62-60.us-west-2.compute.internal>
2021-10-13 01:16:07 +0000

    Test 123

--- js/stripe.js
+++ js/stripe.js
@@ -14,6 +14,7 @@ WildRydes.map = WildRydes.map || {};
     }
     
     function createCheckoutSession() {
+        console.log("Hello!");
         $.ajax({
             method: 'POST',
             url: 'https://749ruwi05l.execute-api.us-west-2.amazonaws.com/prod/checkout-sessions',
